b1ff5903ea47251e16295ffeca95833447a8bfd3
Kaili Wang <kailiwang@Kailis-MBP-2.home>
2021-11-24 21:20:30 -0500

    add react (unfinished)

--- webapp/app/static/js/index.js
+++ webapp/app/static/js/index.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+<<<<<<< HEAD
 import RevealsPage from "../jsx/reveals";
 
 let url = window.location.href.toLowerCase();
@@ -9,3 +10,19 @@ console.log(url);
 if (url.includes("/reveals")) {
     ReactDOM.render(<RevealsPage />, document.getElementById("root"));
 }
+=======
+import App from "../jsx/App";
+
+$(function () {
+
+    var form = $('#address-form');
+    form.submit(e => {
+        e.preventDefault();
+        console.log('awefwef');
+        const a = $( "#input-address" ).first().val()
+        window.location.href = "/cluster?address=" + a;
+    })
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+>>>>>>> b4ef37e (add react (unfinished))
