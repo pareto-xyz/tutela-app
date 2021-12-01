9a1a9569698b793c1c0760309928636e5e0ee201
Mike Wu <me@mikewuis.me>
2021-12-01 22:41:03 +0000

    updates

--- scripts/diff2vec/make_graph.py
+++ scripts/diff2vec/make_graph.py
@@ -2,6 +2,7 @@
 Need to convert the transactions dataframe to a smaller dataframe 
 with the columns: `Address A | Address B | # of interactions`
 """
+import numpy as np
 import pandas as pd
 from typing import Any, Iterable
 
@@ -16,30 +17,35 @@ def yield_transactions(
 
 
 def make_graph_dataframe(
-    transactions_csv: str, 
-    known_addresses_csv: str,
-    chunk_size: int = 10000,
-) -> pd.DataFrame:
-    # we want to ignore these addresses as they are not interesting to 
-    # cluster against.
-    known_addresses: pd.DataFrame = pd.read_csv(known_addresses_csv)
+    transactions_csv: str, out_csv: str, chunk_size: int = 10000) -> pd.DataFrame:
+    count: int = 0
 
+    print('processing txs',  end = '', flush=True)
     for chunk in yield_transactions(transactions_csv, chunk_size):
-        breakpoint()
+        chunk: db.DataFrame = \
+            chunk.groupby(['from_address', 'to_address'], as_index=False).size()
+
+        if count == 0:
+            chunk.to_csv(out_csv)
+        else:
+            chunk.to_csv(out_csv, mode='a', header=False, index=False)
+
+        print('.', end = '', flush=True)
+        count += 1 
 
 
 def main(args: Any):
-    data: pd.DataFrame = make_graph_dataframe(
-        args.transactions_csv, args.known_addresses_csv, args.chunk_size)
+    make_graph_dataframe(
+        args.transactions_csv, args.save_csv, args.chunk_size)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument('transactions_csv', type=str, help='path to transaction data')
-    parser.add_argument('known_addresses_csv', type=str, help='path to known address data')
-    parser.add_argument('save_dir', type=str, help='path to save output')
-    parser.add_argument('--chunk-size', type=int, help='Chunk size (default: 10000)')
+    parser.add_argument('save_csv', type=str, help='path to save data')
+    parser.add_argument('--chunk-size', type=int, default=1000000,
+                        help='Chunk size (default: 1000000)')
     args: Any = parser.parse_args()
 
     main(args)
