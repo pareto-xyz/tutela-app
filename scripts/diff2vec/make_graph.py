51c506f6c79abadfbb65790989f6f95c512c94fe
mhw32 <me@mikewuis.me>
2021-12-01 14:11:50 -0800

    add some stuff

--- scripts/diff2vec/make_graph.py
+++ scripts/diff2vec/make_graph.py
@@ -7,33 +7,39 @@ from typing import Any, Iterable
 
 
 def yield_transactions(
-    transaction_csv: str,
-    chunk_size: int = 10000,
-) -> Iterable[pd.DataFrame]:
+    transactions_csv: str, chunk_size: int = 10000) -> Iterable[pd.DataFrame]:
     """
     Load a segment at a time (otherwise too large).
     """
-    for chunk in pd.read_csv(transaction_csv, chunksize = chunk_size):
+    for chunk in pd.read_csv(transactions_csv, chunksize = chunk_size):
         yield chunk
 
 
 def make_graph_dataframe(
-    transaction_csv: str,
+    transactions_csv: str, 
+    known_addresses_csv: str,
     chunk_size: int = 10000,
 ) -> pd.DataFrame:
-    for chunk in yield_transactions(transaction_csv, chunk_size):
-        chunk.value = chunk.value.astype(float) / 10**18
+    # we want to ignore these addresses as they are not interesting to 
+    # cluster against.
+    known_addresses: pd.DataFrame = pd.read_csv(known_addresses_csv)
+
+    for chunk in yield_transactions(transactions_csv, chunk_size):
+        breakpoint()
 
 
 def main(args: Any):
-    pass
+    data: pd.DataFrame = make_graph_dataframe(
+        args.transactions_csv, args.known_addresses_csv, args.chunk_size)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument('transactions_csv', type=str, help='path to transaction data')
+    parser.add_argument('known_addresses_csv', type=str, help='path to known address data')
     parser.add_argument('save_dir', type=str, help='path to save output')
+    parser.add_argument('--chunk-size', type=int, help='Chunk size (default: 10000)')
     args: Any = parser.parse_args()
 
     main(args)
