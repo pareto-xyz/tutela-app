2e7ca2032cf22beda7e9526db6cf4bfba122f7c9
Kaili Wang <44105002+kkailiwang@users.noreply.github.com>
2021-12-13 20:42:19 -0600

    Donations (#25)
    * add donations address to footer
    
    * move toheader
    
    Co-authored-by: Kaili Wang <kailiwang@kailis-mbp-2.lan>

---
+++ webapp/app/static/jsx/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="container">
+            <div>
+                <a href="https://github.com/TutelaLabs/tutela-app"><i className="fab fa-github"></i></a>
+                <span>
+                    &nbsp; copyright 2021.
+                </span>
+            </div>
+        </footer>
+    )
+}
