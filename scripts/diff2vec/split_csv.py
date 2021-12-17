31f71c7c9a939b7d73341b50758a1abd59e71b6a
mhw32 <me@mikewuis.me>
2021-12-17 08:49:19 -0800

    add split file

---
+++ scripts/diff2vec/split_csv.py
@@ -0,0 +1,35 @@
+"""
+Split giant CSV into many small csvs
+"""
+import os
+import csv
+from typing import List
+
+
+def split(file_loc: str, out_dir: str, file_size=100000):
+    with open(file_loc) as fp:
+        count: int = 0
+        curr_split: int = 0
+        reader = csv.reader(fp)
+        header: List[str] = reader.next()
+        writer = None
+
+        for row in reader:
+            if count % file_size == 0:
+                print(f'parsed {count} rows.')
+                split_filename: str = os.path.join(out_dir, f'edges-{curr_split}.csv')
+                writer = csv.writer(open(split_filename, 'w'))
+                writer.writerow(header)
+                curr_split += 1
+
+            writer.writerow(row)
+            count += 1
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('edges_file', type=str, help='path to csv file containing edges')
+    parser.add_argument('split_dir', type=str, help='path to dump split files')
+    parser.add_argument('--file-size', type=int, default=100000)
+    args = parser.parse_args()
