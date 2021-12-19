c501aec8dd5b293bd39e5316cce5fa4710566a47
Mike Wu <me@mikewuis.me>
2021-12-18 16:44:51 -0800

    add replicates

--- scripts/diff2vec/make_sequences.py
+++ scripts/diff2vec/make_sequences.py
@@ -9,7 +9,11 @@ def main(args: Any):
     graph: UndirectedGraph = UndirectedGraph()
     graph.from_pickle(args.edges_file)
     sequencer: SubGraphSequences = SubGraphSequences(graph, args.cover_size)
-    sequencer.get_sequences(args.components_file, args.sequences_file)
+
+    for i in range(args.replicates):
+        name, ext = os.path.splitext(args.sequences_file)
+        sequences_file: str = f'{name}_rep{i}' + ext
+        sequencer.get_sequences(args.components_file, sequences_file)
 
 
 if __name__ == "__main__":
@@ -20,6 +24,8 @@ if __name__ == "__main__":
     parser.add_argument('sequences_file', type=str, help='path to sequences file')
     parser.add_argument('--cover-size', type=int, default=80, 
                         help='size of subgraph (default: 80)')
+    parser.add_argument('--replicates', type=int, default=1,
+                        help='number of replicates (default: 1)')
     args: Any = parser.parse_args()
 
     main(args)
