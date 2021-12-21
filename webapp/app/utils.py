4a354ac1b7e35d9abe3a4f73c10463c653ca037f
Mike Wu <me@mikewuis.me>
2021-12-21 21:15:52 +0000

    more naming changes

--- webapp/app/utils.py
+++ webapp/app/utils.py
@@ -113,6 +113,8 @@ def get_display_aliases() -> Dict[str, str]:
         'exact_match': 'address match reveals',
         'multi_denom': 'multi-denom reveals',
         'gas_price': 'unique gas price reveals',
+        'linked_tx': 'linked address reveals',
+        'torn_mine': 'TORN mining reveals',
         'unique_gas_price': 'unique gas price',
         'deposit_address_reuse': 'deposit address reuse',
         'multi_denomination': 'multi-denomination',
