565f2d4d732b25e239ce05eabc1553e70de91020
mhw32 <me@mikewuis.me>
2021-12-21 20:57:01 -0800

    simplify script for word2vec

--- scripts/diff2vec/run_word2vec.py
+++ scripts/diff2vec/run_word2vec.py
@@ -1,33 +1,31 @@
-from typing import Any, List
-from src.utils.utils import from_jsonlines
-from src.diff2vec.diff2vec import Diff2Vec
+from typing import Any
+from gensim.models import Word2Vec
 
 
 def main(args: Any):
-    sequences: List[List[int]] = from_jsonlines(args.sequences_file)
-    model: Diff2Vec = Diff2Vec(
-        dimensions = args.dim,
-        window_size = args.window,
-        cover_size = args.cover,
-        epochs = args.epochs,
-        learning_rate = args.lr,
+    model: Word2Vec = Word2Vec(
+        corpus_file = args.corpus_file,
+        vector_size = args.dim,
+        window = args.window,
         workers = args.workers,
+        epochs = args.epochs,
+        alpha = args.lr,
         seed = args.seed,
-        cache_dir = args.cache_dir,
     )
-    model.fit(sequences)
+    print('saving mode file.')
+    model.save(args.model_file)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser: ArgumentParser = ArgumentParser()
-    parser.add_argument('sequences_file', type=str, help='path to cached sequences')
-    parser.add_argument('--epochs', type=int, default=10, help='epochs (default: 10)')
+    parser.add_argument('corpus_file', type=str, help='path to cached sequences')
+    parser.add_argument('model_file', type=str, help='path to save model')
+    parser.add_argument('--epochs', type=int, default=5, help='epochs (default: 5)')
     parser.add_argument('--workers', type=int, default=4, help='workers (default: 4)')
-    parser.add_argument('--lr', type=float, default=0.05, help='learning rate (default: 0.05)')
+    parser.add_argument('--lr', type=float, default=0.025, help='learning rate (default: 0.025)')
     parser.add_argument('--dim', type=float, default=128, help='dimensionality (default: 128)')
-    parser.add_argument('--window', type=float, default=10, help='window (default: 10)')
-    parser.add_argument('--cover', type=float, default=80, help='cover (default: 80)')
+    parser.add_argument('--window', type=float, default=5, help='window (default: 5)')
     parser.add_argument('--seed', type=float, default=42, help='random seed (default: 42)')
     args: Any = parser.parse_args()
 
