ac14e0462e7bf63d7df94c5353b14b0e3ee1533b
Mike Wu <me@mikewuis.me>
2021-12-22 08:59:53 -0800

    make simpler

--- scripts/diff2vec/run_word2vec.py
+++ scripts/diff2vec/run_word2vec.py
@@ -6,7 +6,6 @@ def main(args: Any):
     model: Word2Vec = Word2Vec(
         corpus_file = args.corpus_file,
         vector_size = args.dim,
-        window = args.window,
         workers = args.workers,
         epochs = args.epochs,
         alpha = args.lr,
@@ -23,9 +22,9 @@ if __name__ == "__main__":
     parser.add_argument('model_file', type=str, help='path to save model')
     parser.add_argument('--epochs', type=int, default=5, help='epochs (default: 5)')
     parser.add_argument('--workers', type=int, default=4, help='workers (default: 4)')
+    parser.add_argument('--min-count', type=int, default=5, help='min count (default: 5)')
     parser.add_argument('--lr', type=float, default=0.025, help='learning rate (default: 0.025)')
     parser.add_argument('--dim', type=float, default=128, help='dimensionality (default: 128)')
-    parser.add_argument('--window', type=float, default=5, help='window (default: 5)')
     parser.add_argument('--seed', type=float, default=42, help='random seed (default: 42)')
     args: Any = parser.parse_args()
 
