9c0dbed9b38e8a0c39738880f5ee2e359f0329cc
Mike Wu <me@mikewuis.me>
2021-12-22 09:12:07 -0800

    update

--- scripts/diff2vec/run_word2vec.py
+++ scripts/diff2vec/run_word2vec.py
@@ -1,5 +1,27 @@
+import os
 from typing import Any
 from gensim.models import Word2Vec
+from gensim.models.callbacks import CallbackAny2Vec
+
+
+class TrainCallback(CallbackAny2Vec):
+    """
+    Save model every epoch and log epoch completion.
+    """
+
+    def __init__(self, save_dir: str):
+        self._save_dir: str = save_dif
+        self._epoch: int = 0
+
+    def on_epoch_start(self, _):
+        print(f'epoch {self._epoch} start')
+        self._epoch += 1
+
+    def on_epoch_end(self, model):
+        out_path: str = os.path.join(self._save_dir, f'word2vec-epoch{self._epoch}.model')
+        print(f'epoch {self._epoch} end')
+        model.save(out_path)
+        print(f'saved model to {out_path}')
 
 
 def main(args: Any):
@@ -10,16 +32,15 @@ def main(args: Any):
         epochs = args.epochs,
         alpha = args.lr,
         seed = args.seed,
+        callbacks = [TrainCallback(args.model_dir)],
     )
-    print('saving mode file.')
-    model.save(args.model_file)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument('corpus_file', type=str, help='path to cached sequences')
-    parser.add_argument('model_file', type=str, help='path to save model')
+    parser.add_argument('model_dir', type=str, help='path to save model')
     parser.add_argument('--epochs', type=int, default=5, help='epochs (default: 5)')
     parser.add_argument('--workers', type=int, default=4, help='workers (default: 4)')
     parser.add_argument('--min-count', type=int, default=5, help='min count (default: 5)')
