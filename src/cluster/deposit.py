5f9a1fd57c8fb21c5fbed2e4829aab44f9d44c7f
Mike Wu <me@mikewuis.me>
2021-12-23 16:55:50 +0000

    updates to transactions

--- src/cluster/deposit.py
+++ src/cluster/deposit.py
@@ -175,7 +175,8 @@ class DepositCluster(BaseCluster):
         exchange_addrs: np.array = exchanges.address
         blacklist_addrs: np.array = blacklist.address
 
-        columns: List[str] = ['block_number', 'block_timestamp', 'from_address', 'to_address', 'value']
+        columns: List[str] = [
+            'hash', 'block_number', 'block_timestamp', 'from_address', 'to_address', 'value']
         tx_chunk: pd.DataFrame = tx_chunk[columns].sort_values('block_number')
         tx_chunk['block'] = tx_chunk['block_number']  # dummy column
 
@@ -228,9 +229,10 @@ class DepositCluster(BaseCluster):
 
         # keep transactions from EOA to deposit
         transactions: pd.DataFrame = deposit[
-            ['from_address_x', 'from_address_y', 'block_number_x', 'block_timestamp_x']
+            ['from_address_x', 'from_address_y', 'hash_x', 'block_number_x', 'block_timestamp_x']
         ]
-        transactions.columns = ['user', 'deposit', 'block_number', 'block_timestamp']
+        transactions.columns = \
+            ['user', 'deposit', 'transaction', 'block_number', 'block_timestamp']
         transactions: pd.DataFrame = transactions.drop_duplicates()
 
         return results, transactions
