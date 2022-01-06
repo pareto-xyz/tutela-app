f01069c72302f39499233270f1f7dad675115653
mhw32 <me@mikewuis.me>
2022-01-05 21:59:48 -0800

    nice!

--- scripts/tornadocash/bq_commands.sh
+++ scripts/tornadocash/bq_commands.sh
@@ -7,6 +7,6 @@ bq mk --schema ./data/static/tcash/traces_schema.json lexical-theory-329617:torn
 bq mk --schema ./data/static/tcash/transactions_schema.json lexical-theory-329617:tornado_transactions.transactions
 
 # make tornado traces table
-bq query --destination_table lexical-theory-329617:tornado_transactions.traces --use_legacy_sql=false 'select * from bigquery-public-data.crypto_ethereum.traces where to_address in (select address from lexical-theory-329617:tornado_transactions.tornadocontracts)) and substr(input, 1, 10) in ("0xb214faa5", "0x21a0adb6")'
+bq query --destination_table lexical-theory-329617:tornado_transactions.traces --use_legacy_sql=false 'select * from bigquery-public-data.crypto_ethereum.traces where (to_address in (select address from lexical-theory-329617.tornado_transactions.tornadocontracts)) and substr(input, 1, 10) in ("0xb214faa5", "0x21a0adb6")'
 # make tornado transactions table
-bq query --destination_table lexical-theory-329617:tornado_transactions.transactions --use-use_legacy_sql=false 'select * from bigquery-public-data.crypto_ethereum.transactions where hash in (select transaction_hash from tornado_transactions.traces)'
+bq query --destination_table lexical-theory-329617:tornado_transactions.transactions --use_legacy_sql=false 'select * from bigquery-public-data.crypto_ethereum.transactions as b where b.hash in (select transaction_hash from lexical-theory-329617.tornado_transactions.traces)'
