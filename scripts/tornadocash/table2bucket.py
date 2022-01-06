8f892c04f1f1e08b853279d5b70caa322937d6c0
mhw32 <me@mikewuis.me>
2022-01-05 22:09:01 -0800

    fixes

--- scripts/tornadocash/table2bucket.py
+++ scripts/tornadocash/table2bucket.py
@@ -11,11 +11,14 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--project', type=str, default='lexical-theory-329617.tornado_transactions')
+    parser.add_argument('--project', type=str, default='lexical-theory-329617.tornado_transactions', 
+                        help='default: lexical-theory-329617.tornado_transactions')
     parser.add_argument('--bucket', type=str, default='traces',
-                        choices=['traces', 'transactions', 'tornadocontracts'])
+                        choices=['traces', 'transactions', 'tornadocontracts'],
+                        help='default: traces')
     parser.add_argument('--table', type=str, default='tornado-trace',
-                        choices=['tornado-trace', 'tornado-transaction', 'tornado-contract'])
+                        choices=['tornado-trace', 'tornado-transaction', 'tornado-contract'],
+                        help='default: tornado-trace')
     args: Any = parser.parse_args()
 
     main(args)
