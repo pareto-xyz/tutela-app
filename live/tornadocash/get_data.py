f483a79e7ff394a4b685a02b0a62895d81812ae2
mhw32 <me@mikewuis.me>
2022-01-05 23:47:11 -0800

    up

--- live/tornadocash/get_data.py
+++ live/tornadocash/get_data.py
@@ -1,3 +1,22 @@
 """
+Contains utilities to download recent data needed to run heuristics
+on tornado cash data.
 
+Most of the heuristics require access to all of tornado cash at once.
+As such, we cannot just download a daily slice (an increment) and 
+process that. The best we can do is the following:
+
+1) Keep a table of Tornado Cash trace and transaction data in BigQuery. 
+We will update this table daily. 
+
+2) Once updated, the table will be exported a Google cloud bucket. We 
+will delete prior contents in bucket before exporting.
+
+3) Download the trace and transactions buckets locally and rename to 
+expected tornado_trace.csv and tornado_transaction.csv.
+
+4) Run the preprocessing script to generate complete_withdraw_tx.csv
+and complete_deposit_tx.csv. 
+
+5) Save to disk (overwrite old files).
 """
---
+++ live/tornadocash/get_heuristics.py
@@ -0,0 +1,19 @@
+"""
+Contains utilities to compute heuristics on all tornado cash data.
+
+1) This assumes get_data.py has been run. It will need access to 
+updated files complete_withdraw_tx.csv and complete_deposit_tx.csv. 
+
+2) Run the following heuristics in order:
+    - ExactMatch
+    - UniqueGasPrice
+    - SameNumTx
+    - LinkedTx
+    - TornMine
+
+3) Run json_to_sql_format functions to generate a processed.csv for 
+each heuristic. We will not write any other files to disk.
+
+4) Delete existing content in tables. Insert new CSV files into table.
+If possible look into ovewriting here rather than deleting rows.
+"""
