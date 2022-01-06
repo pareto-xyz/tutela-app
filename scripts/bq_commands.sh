39eb4c7078761ce01ef0c9328f1a1fe14d72b1ac
mhw32 <me@mikewuis.me>
2022-01-05 21:48:51 -0800

    add bq commands for tcash data

--- scripts/bq_commands.sh
+++ scripts/bq_commands.sh
@@ -7,6 +7,6 @@ bq --location=US query --destination_table lexical-theory-329617:crypto_ethereum
 # get txs from 1 year
 bq --location=US query --destination_table lexical-theory-329617:crypto_ethereum.transactions_1year --use_legacy_sql=false "select from_address, to_address, value, block_timestamp, block_number from bigquery-public-data.crypto_ethereum.transactions where (block_number >= 10966874) and (block_number <= 13330090)"
 
-# get all txs before adte
+# get all txs before date
 bq --location=US query --destination_table lexical-theory-329617:crypto_ethereum.transactions3 --use_legacy_sql=false "select b.from_address, b.to_address, b.hash as transaction, b.value, b.block_timestamp, b.block_number from bigquery-public-data.crypto_ethereum.transactions as b where b.block_number <= 13330090"
 
---
+++ scripts/tornadocash/bq_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# load contract table into bigquery (to be run once)
+bq load lexical-theory-329617:tornado_transactions.tornadocontracts ./data/static/tcash/tornadocontracts.csv ./data/static/tcash/tornadocontracts_schema.json
+# make (empty) traces and transactions table
+bq mk --schema ./data/static/tcash/traces_schema.json lexical-theory-329617:tornado_transactions.traces
+bq mk --schema ./data/static/tcash/transactions_schema.json lexical-theory-329617:tornado_transactions.transactions
+
+bq --location=US query --destination_table lexical-theory-329617:tornado_transactions.traces --use_legacy_sql=false 'select * from bigquery-public-data.crypto_ethereum.traces where to_address in (select address from lexical-theory-329617:tornado_transactions.tornadocontracts)) and substr(input, 1, 10) in ("0xb214faa5", "0x21a0adb6")'
