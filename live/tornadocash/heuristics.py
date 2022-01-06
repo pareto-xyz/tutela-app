dffdb95009640ec09b6e69533a7e9c7007cfe4cf
mhw32 <me@mikewuis.me>
2022-01-06 11:24:03 -0800

    updates

--- live/tornadocash/heuristics.py
+++ live/tornadocash/heuristics.py
@@ -14,6 +14,6 @@ updated files complete_withdraw_tx.csv and complete_deposit_tx.csv.
 3) Run json_to_sql_format functions to generate a processed.csv for 
 each heuristic. We will not write any other files to disk.
 
-4) Delete existing content in tables. Insert new CSV files into table.
+4) Delete existing content in db. Insert new CSV files into db.
 If possible look into ovewriting here rather than deleting rows.
 """
---
+++ live/tornadocash/run.py
@@ -0,0 +1,9 @@
+from live.tornadocash import data, heuristics
+
+
+def main():
+    data.main(); heuristics.main()
+
+
+if __name__ == "__main__":
+    main()
