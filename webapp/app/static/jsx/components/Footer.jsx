0d5338b1644439ce7843333e95a1c978aa5e2d05
Kaili Wang <kailiwang@DNa811f1a.SUNet>
2022-01-19 20:40:14 -0800

    whitepaper

--- webapp/app/static/jsx/components/Footer.jsx
+++ webapp/app/static/jsx/components/Footer.jsx
@@ -4,10 +4,12 @@ export default function Footer() {
     return (
         <div className="footer-react col-12">
             <footer>
+                <span><a href="https://arxiv.org/abs/2201.06811">whitepaper </a>|&nbsp;</span>
                 <a href="https://github.com/TutelaLabs/tutela-app"><i className="fab fa-github"></i></a>
                 <span>
-                    &nbsp; copyright © 2021
+                &nbsp;| copyright © 2021
                 </span>
+                
             </footer>
         </div>
     )
