f630f5b21c67c4b4fda4be9dea227798b4cf096d
Mike Wu <me@mikewuis.me>
2024-01-22 14:04:33 -0800

    more deletions

--- webapp/staticapp/static/jsx/components/AddressSearchBar.jsx
+++ webapp/staticapp/static/jsx/components/AddressSearchBar.jsx
@@ -2,9 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState, useRef } from 'react';
 import { InputGroup, FormControl, Form, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { isValid } from '../../js/utils';
-//import isTornado from '../../data/istornado';
 
-export default function AddressSearchBar({ onSubmit, inputAddress, setInputAddress,
+export default function AddressSearchBar({ inputAddress, setInputAddress,
     myClassName = "fixed-width mb-1 col-sm-12 col-md-6 col-lg-8",
     showTornadoHovers = true }) {
 
@@ -31,7 +30,6 @@ export default function AddressSearchBar({ onSubmit, inputAddress, setInputAddre
             return;
         }
         setInvalid(false);
-        onSubmit(addr);
     }
 
     const onChangeInputAddress = e => {
@@ -71,7 +69,7 @@ export default function AddressSearchBar({ onSubmit, inputAddress, setInputAddre
                 overlay={renderTcashTooltip}
             >
 
-                <InputGroup onSubmit={submitInputAddress} className="fixed-width col-md-12 col-lg-8" hasValidation>
+                <InputGroup className="fixed-width col-md-12 col-lg-8" hasValidation>
                     <FormControl onKeyPress={(e) => {
                         if (e.key !== 'Enter') return;
                         e.preventDefault();
@@ -86,7 +84,9 @@ export default function AddressSearchBar({ onSubmit, inputAddress, setInputAddre
                     >
                     </FormControl>
 
-                    <InputGroup.Text onClick={() => submitInputAddress(inputEl.current.value)} className="right-submit-icon"><img width="15" src="/static/img/loupe.svg" alt="search"></img> </InputGroup.Text>
+                    <InputGroup.Text className="right-submit-icon">
+                        <img width="15" src="/static/img/loupe.svg" alt="search"></img> 
+                    </InputGroup.Text>
                     <Form.Control.Feedback type="invalid">
                         Please enter a valid ethereum address or .eth ens name.
                     </Form.Control.Feedback>
